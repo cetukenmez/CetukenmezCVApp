1374cf74c78f92bb4285016bc0f5a724ea0498e6
Can Erdem TUKENMEZ <cetukenmez@gmail.com>
2024-12-04 19:25:43 +0300

    Add project files.

---
+++ CetukenmezCVApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CetukenmezCVApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
